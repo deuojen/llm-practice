502ac209d4bdbc6122c05e75b7973b628b0b7714
nuri naim <deuojen@gmail.com>
2026-06-02 16:53:58 +0100

    complete loop

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,9 @@
 import os
 
 
+from google.genai import types
+
+
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
         abs_working_dir: str = os.path.abspath(working_directory)
@@ -13,7 +16,7 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
             return f'Error: "{directory}" is not a directory'
 
         # return f'Success: "{directory}" is within the working directory'
-        files_info = list[str] = []
+        files_info = []  # list[str] = []
         for filename in os.listdir(target_dir):
             filepath = os.path.join(target_dir, filename)
             is_dir = os.path.isdir(filepath)
@@ -26,3 +29,18 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
 
     except Exception as e:
         return f"Error listing files: {e}"
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
